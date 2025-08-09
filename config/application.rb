d20030f2b1554dcd86880842fe9da0157e546f63
Kirill Biller <biller.k@yandex.ru>
2025-08-09 22:39:34 +0300

    Updated Rails application configuration to load defaults for version 8.0 and changed time zone setting to Moscow.

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ Bundler.require(*Rails.groups)
 module ActOn
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.2
+    config.load_defaults 8.0
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
@@ -33,7 +33,7 @@ module ActOn
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Moscow"
     # config.eager_load_paths << Rails.root.join("extras")
 
     # Don't generate system test files.
